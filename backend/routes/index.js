155f8f32f4f874af71013f067e10117ff9a799d2
thanusree45 <143024576+thanusree45@users.noreply.github.com>
2024-09-17 10:40:42 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const axios = require('axios');
 const Crypto = require('../models/Crypto');
 
@@ -8,18 +7,6 @@ const Crypto = require('../models/Crypto');
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'views' });
 });
-
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('MongoDB connected...');
-    // Start fetching data every 5 minutes
-    setInterval(fetchCryptoData, 5 * 60 * 1000); // 5 minutes
-    fetchCryptoData(); // Initial fetch
-  })
-  .catch(err => console.error('MongoDB connection error:', err));
 // Route to fetch top 10 cryptocurrencies from MongoDB
 router.get('/api/getTop10', async (req, res) => {
   try {
